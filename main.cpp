0c1fbb48382bee8b027327112e6264531623a02a
Martin Wikström <marrewikstro@gmail.com>
2010-08-18 11:34:34 +0300

    Made it display a window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 #include "SDL/SDL.h"
+#include "SDL/SDL_gfxPrimitives.h"
 #include <cmath>
 
 int main(int argc,char* args[]){
   
+  //Get it going
+  SDL_Init(SDL_INIT_VIDEO);
+  
+  //This is the windows
+  SDL_Surface* win = SDL_SetVideoMode(800,600,32,SDL_HWSURFACE|SDL_DOUBLEBUF);
+  
+  //Set the window title
+  SDL_WM_SetCaption("Red Roids",NULL);
+  
+  //Game loop
+  SDL_Event event;
+  bool inloop=true;
+  while(inloop){
+    while(SDL_PollEvent(&event)){
+      if(event.type==SDL_QUIT) inloop=false;
+    }
+    
+    //Swap double buffer
+    SDL_Flip(win);
+  }
+  
+  
+  //Clean up
+  SDL_Quit();
   
   //Done
   return 0;
