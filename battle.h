119564d5145d8ad4e07985807cf8c87fd52183b0
Martin Wikström <marrewikstro@gmail.com>
2012-03-26 21:51:23 +0300

    Zoom level depending on window size and velocity

--- battle.h
+++ battle.h
@@ -10,13 +10,33 @@
 class RR_battle {
 public:
     RR_unit a[RR_BATTLE_MAX_UNITS]; // Units currently on battlefield
-    RR_vec2 cam;
+    RR_vec2 cam, cam_trg;
+    double zoom, zoom_trg;
+    
+    // Constructor
+    RR_battle() {
+        cam = RR_vec2();
+        cam_trg = cam;
+        zoom = 0.001;
+        zoom_trg = zoom;
+    }
     
     // Main battle loop
     bool main(SDL_Surface* win, float fspd, Uint8* keys) {
-        RR_vec2 cam_fix;
-        int cam_count = 0;
+        
+        // Center camera on player
+        cam_trg = a[0].pos + a[0].spd * 0.5;
+        zoom_trg = ((double(RR_g.wid + RR_g.hgt) / 2.0) / 800.0) / (1.0 + RR_g_vec2.distance(RR_vec2(), a[0].spd) * 0.001);
+        
+        // Smooth camera transitions
+        cam = cam + (cam_trg - cam) * 0.04;
+        zoom = zoom + (zoom_trg - zoom) * 0.04;
+        
+        // Loop through ships
         for(int i = 0; i < RR_BATTLE_MAX_UNITS; i++) if(a[i].in_use) {
+            
+            // Display ship
+            a[i].draw(win, (a[i].pos - cam) * zoom + RR_vec2(RR_g.cntx, RR_g.cnty), a[i].nrm, zoom);
             
             // Player controls this ship
             if(i == 0) a[i].player_input(keys);
@@ -39,19 +59,7 @@ public:
             
             // Move ships
             a[i].move(fspd);
-            
-            // Display ships
-            a[i].draw(win, a[i].pos - cam + RR_vec2(RR_g.cntx, RR_g.cnty), a[i].nrm, 1.0);
-            
-            // Center camera mostly on player
-//             cam_count++;
-//             cam_fix = cam_fix + a[i].pos;
-            if(i == 0) {
-                cam_fix = cam_fix + a[i].pos * 10.0;
-                cam_count += 10;
-            }
         } else a[i] = RR_unit(rand() % 6, RR_g_vec2.box_random() * 1000.0);
-        if(cam_count) cam = cam_fix / double(cam_count);
         
         // Done
         return 0;
